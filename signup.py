b61701b30074b796055a70fd7978b009055a6e02
Soren Rood <sorenrood@gmail.com>
2020-09-28 02:46:43 -0700

    started talking to slack api for email signup

--- signup.py
+++ signup.py
@@ -4,3 +4,4 @@ from slack import WebClient
 client = WebClient(token=os.environ["SLACK_API_TOKEN"])
 response = client.conversations_list()
 conversations = response["channels"]
+print(conversations)
