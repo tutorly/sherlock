86d931ce661c24caeb02fda85b36243d5e405cc0
Soren Rood <sorenrood@gmail.com>
2020-09-28 01:58:37 -0700

    got session working

--- session.py
+++ session.py
@@ -21,10 +21,8 @@ class Session():
         self.dates = []
 
     def run(self):
-        while(1):
-            print('Starting up the boosters...')
-            time.sleep(5)
-            pass
+        print('Starting up the boosters')
+        self.doScrape()
 
     def getStoredCases(self):
         """
@@ -64,5 +62,51 @@ class Session():
         This function will write the last scraped value to the txt file.
         """
         f = open('cases.txt', 'w')
-        f.write(cases)
+        f.write(str(cases))
         f.close()
+
+    def doScrape(self):
+        # Setup for lxml
+        page = requests.get(self.url)
+        tree = html.fromstring(page.content)
+
+        # This is the path to the div that stores the updated data
+        path_to_cases = '//*[@id="pageBody"]/div/p/text()'
+        path_to_dates = '//*[@id="pageBody"]/div/p/strong/text()'
+
+        # Get cases and add to cases list
+        for element in tree.xpath(path_to_cases):
+            self.cases.append(element)
+
+        # Get dates and append to dates list
+        for element in tree.xpath(path_to_dates):
+            self.dates.append(element)
+
+        # Check to make sure length is the same for both lists
+        if len(self.cases) != len(self.dates):
+            sys.exit('ERROR. Cases list length: {}. Dates list length: {}'.format(
+                len(self.cases), len(self.dates)))
+
+        # Store the number of cases
+        current_num_cases = len(self.cases)
+        print('current number of cases: {}'.format(current_num_cases))
+
+        if int(self.getStoredCases()) != current_num_cases:
+            self.sendEmail()
+            self.setNumCases(current_num_cases)
+
+        # Cases Output
+        for x in range(0, current_num_cases):
+            print('{}: {}'.format(self.dates[x], self.cases[x]))
+
+        # Get the date from SPU website
+        spu_last_updated = str(tree.xpath(
+            '//*[@id="pageBody"]/div/p[6]/em/text()'))
+        date_spu_last_updated = spu_last_updated.strip("['Last updated: ']",)
+
+        # Format today's date
+        today = str(date.today()).split('-')
+        today = '{}/{}/{}'.format(today[1], today[2], today[0])
+
+        print('Last Update From SPU: ', date_spu_last_updated)
+        print('Last Update From Tutorly: ', today)
