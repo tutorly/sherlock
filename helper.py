6649f82b0d467d322ce9ac23879131c2373ac760
Soren Rood <sorenrood@gmail.com>
2020-09-28 00:24:57 -0700

    fixed increment cases function

--- helper.py
+++ helper.py
@@ -1,15 +1,14 @@
 import time
 
+
 def incrementCases():
+    """
+    This function will read the .txt file and increase it's value by one. 
+    It is to be used whenever we have found a new COVID-19 case on campus
+    """
     f = open('cases.txt')
     count = f.read()
     count = int(count) + 1
     f.close()
-    f = open('count.txt', 'w')
+    f = open('cases.txt', 'w')
     f.write(str(count))
-
-
-for x in range(0,100):
-    incrementCases()
-    time.sleep(1)
-
