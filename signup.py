432df80d0dce93455f9a2d2f4e1c9a3c30af94de
Soren Rood <sorenrood@gmail.com>
2020-09-28 22:06:38 -0700

    output emails containing an @ sign

--- signup.py
+++ signup.py
@@ -2,11 +2,13 @@ import os
 import requests
 import json
 
+# Slack API request and JSON load
 payload = {'channel': 'G01BE5PUFCM', 'token': os.getenv('SLACK_API_TOKEN')}
 content = requests.get('https://slack.com/api/conversations.history', params=payload).content
 rawjson = json.loads(content)
 messages = rawjson['messages']
 
-# Loop through each message
+# Loop through each message and handle new emails
 for element in messages:
-    print(element['text']) # TODO handle emails here
+    if '@' in element['text']:
+        print(element['text'])
