5b747066de25bb561af01da9dbb3560162aea640
Soren Rood <sorenrood@gmail.com>
2020-09-28 21:31:52 -0700

    Turned slack api request into an object. We can now read the emails.

--- signup.py
+++ signup.py
@@ -3,6 +3,10 @@ import requests
 import json
 
 payload = {'channel': 'G01BE5PUFCM', 'token': os.getenv('SLACK_API_TOKEN')}
+content = requests.get('https://slack.com/api/conversations.history', params=payload).content
+rawjson = json.loads(content)
+messages = rawjson['messages']
 
-resp = requests.get('https://slack.com/api/conversations.history', params=payload)
-print(resp.content)
+# Loop through each message
+for element in messages:
+    print(element['text'])
