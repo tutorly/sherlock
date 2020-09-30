28a0427bf3bf39fdb6052ce7aa6389eef9ec0601
Soren Rood <sorenrood@gmail.com>
2020-09-30 12:06:45 -0700

    removed blank cells from dataframe

--- gtest.py
+++ gtest.py
@@ -5,14 +5,17 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
 import pandas as pd
+import numpy as np
 
 # Connect with our google sheet. The creds.json is hidden by default. Soren has access to it. 
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
 client = gspread.authorize(creds)
 sheet = client.open("SPU COVID-19 Tracking")
-sherlock = sheet.worksheet('Sheet4')
+sherlock = sheet.worksheet('emails')
 
 # Use pandas here to work with large data
 df = pd.DataFrame(sherlock.get_all_records())
+df = df.replace('', np.nan)
+df = df.dropna()
 print(df)
