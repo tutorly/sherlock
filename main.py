df3064eb388876ae30926f69f1ddde0a188fa97b
Soren Rood <sorenrood@gmail.com>
2020-10-03 01:49:36 -0700

    docs

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-"""
-This file will loop and check for new cases
-"""
-from scraper import Scraper
+"""This file will loop and check for new cases"""
 from session import Session
-from courier import Courier
 
 session = Session()
-
-# Soren's testing
 session.run()
 
