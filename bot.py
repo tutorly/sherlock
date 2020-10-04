040916c5aa6053a504e130516cc73dc793f0ef7f
Soren Rood <sorenrood@gmail.com>
2020-10-04 14:55:22 -0700

    added logic for test and prod mode

--- bot.py
+++ bot.py
@@ -23,16 +23,25 @@ class Bot():
         self.validator = Validator()
         self.courier = Courier()
         self.loopInterval = 60 # In seconds
+        self.currentMode = Mode.TEST # This is set in self.ModeSelect() method in the run method. I have it set to Mode.TEST default incase someone removes self.modeSelect().
 
     def run(self):
         '''This is the method that drives the entire program.'''
-        self.modeSelect()
+        # Ask the user what mode they want to run the bot in.
+        self.currentMode = self.modeSelect()
         while(1):
             print('-------------')
             self.scraper.scrape()
+
             if self.validator.checkForNewCase():
-                print('NEW CASE FOUND')
-                Courier.sendEmailsToAdminOnly('New case message')
+                print('Sherlock has found a new case.')
+
+                if self.currentMode == Mode.TEST:
+                    Courier.sendEmailsToAdminOnly('This is a test.')
+                
+                if self.currentMode == Mode.PROD:
+                    Courier.sendEmailsToEveryoneInMailingList('New covid-19 case confirmed on campus.')
+
             self.cleanUp()
             print('-------------')
             time.sleep(self.loopInterval)
@@ -42,12 +51,17 @@ class Bot():
         # List of available modes.
         modes = ['test', 'prod']
 
+        # Loop until the user has inputted a correct mode.
         while(1):
             selected_mode = input('Enter "test" for test mode or "prod" for production mode. ')
             if selected_mode in modes:
                 print(f'you entered "{selected_mode}". If this is correct, press ENTER. If not, press control + c to cancel.')
                 input()
-                return
+                if selected_mode == 'test':
+                    return Mode.TEST
+                if selected_mode == 'prod':
+                    return Mode.PROD
+
             else: print(f'{selected_mode} is not valid mode.')
 
     def cleanUp(self):
