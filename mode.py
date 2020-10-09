ea85759052577baee32ce02598e995da509b16f8
Soren Rood <sorenrood@gmail.com>
2020-10-08 20:28:08 -0700

    changed mode to environment

--- mode.py
+++
@@ -1,6 +0,0 @@
-from enum import Enum
-
-class Mode(Enum):
-    '''This class is simply an enum to keep track of what mode our bot is currently in. The numbers are entirely arbitrary.'''
-    TEST = 0
-    PROD = 1
